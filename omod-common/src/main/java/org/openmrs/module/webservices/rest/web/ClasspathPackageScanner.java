63c41d53fa4f5a6663330c4c74f8135a70153396
TheLobbyPigeon <gijsvanvugt2002@gmail.com>
2026-07-02 15:24:08 +0200

    Sonarcube issues

--- omod-common/src/main/java/org/openmrs/module/webservices/rest/web/ClasspathPackageScanner.java
+++ omod-common/src/main/java/org/openmrs/module/webservices/rest/web/ClasspathPackageScanner.java
@@ -14,106 +14,100 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class ClasspathPackageScanner {
-	
-	public static ArrayList<Class<?>> getClassesForPackage(String pkgname, String suffix) throws IOException {
-		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
-		
-		String relPath = pkgname.replace('.', '/');
-		Enumeration<URL> resources = OpenmrsClassLoader.getInstance().getResources(relPath);
-		while (resources.hasMoreElements()) {
-			URL resource = resources.nextElement();
-			if (resource == null) {
-				throw new RuntimeException("No resource for " + relPath);
-			}
-			
-			File directory = null;
-			try {
-				directory = new File(resource.toURI());
-			}
-			catch (URISyntaxException e) {
-				throw new RuntimeException(
-				        pkgname + " (" + resource
-				                + ") does not appear to be a valid URL / URI.  Strange, since we got it from the system...",
-				        e);
-			}
-			catch (IllegalArgumentException ex) {
-				directory = null;
-			}
-			
-			if (directory != null && directory.exists()) {
-				scanDirectory(directory, pkgname, suffix, classes);
-			} else {
-				scanJar(resource, relPath, pkgname, suffix, classes);
-			}
-		}
-		
-		return classes;
-	}
-	
-	private static void scanDirectory(File directory, String pkgname, String suffix, ArrayList<Class<?>> classes) {
-		String[] files = directory.list();
-		if (files == null) {
-			return;
-		}
-		for (String file : files) {
-			if (file.endsWith(suffix)) {
-				String className = pkgname + '.' + file.substring(0, file.length() - 6);
-				try {
-					Class<?> cls = Class.forName(className);
-					if (!cls.isInterface()) {
-						classes.add(cls);
-					}
-				}
-				catch (ClassNotFoundException e) {
-					throw new RuntimeException("ClassNotFoundException loading " + className);
-				}
-			}
-		}
-	}
-	
-	private static void scanJar(URL resource, String relPath, String pkgname, String suffix, ArrayList<Class<?>> classes) throws IOException {
-		String fullPath = resource.getFile();
-		String jarPath = fullPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
-		JarFile jarFile = null;
-		try {
-			jarFile = new JarFile(jarPath);
-			
-			Enumeration<JarEntry> entries = jarFile.entries();
-			while (entries.hasMoreElements()) {
-				JarEntry entry = entries.nextElement();
-				String entryName = entry.getName();
-				
-				if (!entryName.endsWith(suffix)) {
-					continue;
-				}
-				
-						String className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
-						try {
-							Class<?> cls = Class.forName(className);
-							if (!cls.isInterface()) {
-								classes.add(cls);
-							}
-						}
-						catch (ClassNotFoundException e) {
-							throw new RuntimeException("ClassNotFoundException loading " + className);
-						}
-					addClassIfConcrete(className, classes);
-				}
-			}
-		}
-				throw new RuntimeException(pkgname + " (" + resource + ") does not appear to be a valid package", e);
-			}
-			finally {
-				if (jarFile != null) {
-					jarFile.close();
-				}
-			throw new IllegalStateException("ClassNotFoundException loading " + className, e);
-		}
 
-	}
+    private ClasspathPackageScanner() {
+    }
+
+    public static List<Class<?>> getClassesForPackage(String pkgname, String suffix) throws IOException {
+        ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
+
+        String relPath = pkgname.replace('.', '/');
+        Enumeration<URL> resources = OpenmrsClassLoader.getInstance().getResources(relPath);
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            if (resource == null) {
+                throw new IllegalArgumentException("No resource for " + relPath);
+            }
+
+            File directory = null;
+            try {
+                directory = new File(resource.toURI());
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(
+                        pkgname + " (" + resource
+                                + ") does not appear to be a valid URL / URI.  Strange, since we got it from the system...",
+                        e);
+            } catch (IllegalArgumentException ex) {
+                directory = null;
+            }
+
+            if (directory != null && directory.exists()) {
+                scanDirectory(directory, pkgname, suffix, classes);
+            } else {
+                scanJar(resource, relPath, suffix, classes);
+            }
+        }
+
+        return classes;
+    }
+
+    private static void scanDirectory(File directory, String pkgname, String suffix, ArrayList<Class<?>> classes) {
+        String[] files = directory.list();
+        if (files == null) {
+            return;
+        }
+        for (String file : files) {
+            if (file.endsWith(suffix)) {
+                String className = pkgname + '.' + file.substring(0, file.length() - suffix.length());
+                try {
+                    Class<?> cls = Class.forName(className);
+                    if (!cls.isInterface()) {
+                        classes.add(cls);
+                    }
+                } catch (ClassNotFoundException e) {
+                    throw new IllegalArgumentException("ClassNotFoundException loading " + className, e);
+                }
+            }
+        }
+    }
+
+    private static void scanJar(URL resource, String relPath, String suffix, ArrayList<Class<?>> classes) throws IOException {
+        String fullPath = resource.getFile();
+        int bang = fullPath.indexOf("!/");
+        String jarPath = (bang == -1) ? fullPath : fullPath.substring(0, bang);
+        if (jarPath.startsWith("file:")) {
+            try {
+                jarPath = new URL(jarPath).toURI().getPath();
+            } catch (URISyntaxException e) {
+                jarPath = jarPath.replaceFirst("file:", "");
+            }
+        }
+        try (JarFile jarFile = new JarFile(jarPath)) {
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String entryName = entry.getName();
+                if (!entryName.startsWith(relPath) || !entryName.endsWith(suffix)) {
+                    continue;
+                }
+                String className = entryName.replace('/', '.').replace('\\', '.');
+                className = className.substring(0, className.length() - suffix.length());
+                try {
+                    Class<?> cls = Class.forName(className);
+                    if (!cls.isInterface()) {
+                        classes.add(cls);
+                    }
+                } catch (ClassNotFoundException e) {
+                    throw new IllegalArgumentException("ClassNotFoundException loading " + className, e);
+                }
+            }
+        }
+    }
+}
